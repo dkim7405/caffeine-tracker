ea533b52a7abbb4faf52e7a25c5620f5813e664f
Leo Kim <kimd7@rose-hulman.edu>
2025-04-30 22:47:29 -0400

    test

--- app/server/main.py
+++ app/server/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, send_from_directory
 from db_conn import db_conn
 
-app = Flask(__name__, static_folder='../client/dist', static_url_path='/')
+app = Flask(__name__)
 db = db_conn()
 
 @app.route('/')
 def home():
-    return send_from_directory(app.static_folder, 'index.html')
+    return "<h1>Welcome to the Flask App!</h1>"
 
 if __name__ == '__main__':
     app.run(
